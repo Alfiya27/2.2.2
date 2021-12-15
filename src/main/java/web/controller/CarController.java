c2b5ee043666c78fa2e6260afd126240798eaf42
Alfiya27 <irminia27@gmail.com>
2021-12-16 02:54:08 +0300

    перенесла создание машин в service

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -21,17 +21,7 @@ public class CarController {
 
     @GetMapping("/cars")
     public String carList(@RequestParam(value = "count",defaultValue = "5") int count, Model model) {
-        List<Car> list = new ArrayList<>();
-
-            list.add(new Car("Peugeout", "white", 1995));
-            list.add(new Car("DeLorean", "steel", 1981));
-            list.add(new Car("Ford", "black", 1973));
-            list.add(new Car("BMW", "black", 1995));
-            list.add(new Car("Chevrolet", "yellow", 2011));
-            list = carService.carCount(list, count);
-
-
-        model.addAttribute("cars", list);
+        model.addAttribute("cars", carService.carList(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -3,13 +3,21 @@ package web.service;
 import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class CarService {
+    public static List<Car> carList(int count) {
+        List<Car> list = new ArrayList<>();
 
-    public static List<Car> carCount(List<Car> list, int count) {
+        list.add(new Car("Peugeout", "white", 1995));
+        list.add(new Car("DeLorean", "steel", 1981));
+        list.add(new Car("Ford", "black", 1973));
+        list.add(new Car("BMW", "black", 1995));
+        list.add(new Car("Chevrolet", "yellow", 2011));
+
         if (count == 0 || count >= 5) {
             return list;
         }
